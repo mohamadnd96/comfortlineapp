b75ac623f171286a75ed58d8f9de750b3a2fe6b8
Elie Bernuit <bernuit.elie@gmail.com>
2023-09-05 23:55:54 +0200

    Web Implementation

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,25 @@ import 'package:comfortline/material.dart';
 import 'package:comfortline/welcome.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  if(kIsWeb) {
+    await Firebase.initializeApp(options: const FirebaseOptions(
+    apiKey: "AIzaSyAs1fs9uYPu483Hn8ErMK4e52z1akC4mck",
+    authDomain: "test1-8f077.firebaseapp.com",
+    databaseURL: "https://test1-8f077-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "test1-8f077",
+    storageBucket: "test1-8f077.appspot.com",
+    messagingSenderId: "145770969403",
+    appId: "1:145770969403:web:41dc4add13b9411d599f1e",
+    measurementId: "G-918D3N6JPF"
+    ));
+  } else {
+    await Firebase.initializeApp();
+  }
   runApp(const MyApp());
 }
 
